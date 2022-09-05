2b3b1b57efe33789cbd65a9502256f2557d36320
Jvictor-7 <Jvcl@ic.ufal.br>
2022-09-05 17:18:47 -0300

    First Commit

---
+++ Aula 1 - Calculadora de média/js/script.js
@@ -0,0 +1,20 @@
+var nome = "Guilherme";
+var notaDoPrimeiroBimestre = 9;
+var notaDoSegundoBimestre = 3.2;
+var notaDoTerceiroBimestre = 7;
+var notaDoQuartoBimestre = 7;
+
+var somaDasNotas =
+  notaDoPrimeiroBimestre +
+  notaDoSegundoBimestre +
+  notaDoTerceiroBimestre +
+  notaDoQuartoBimestre;
+
+var notaFinal = somaDasNotas / 4;
+
+var notaFixada = notaFinal.toFixed(1);
+
+console.log(`Bem vindo, ${nome}! Você teve uma nota final de: ${notaFixada}`);
+
+// Revisão
+// variáveis, strings, console.log, toFixed, oprerações, matemáticas, concatenação
