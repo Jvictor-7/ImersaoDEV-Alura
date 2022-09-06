47435fd4c708bca35f5cd09fba07ec757f2d01f7
Jvictor-7 <Jvcl@ic.ufal.br>
2022-09-06 07:57:42 -0300

    Adicionado inputs ao projeto.

--- Aula 1 - Calculadora de média/js/script.js
+++ Aula 1 - Calculadora de média/js/script.js
@@ -1,20 +1,30 @@
 var nome = "Guilherme";
-var notaDoPrimeiroBimestre = 9;
-var notaDoSegundoBimestre = 3.2;
-var notaDoTerceiroBimestre = 7;
-var notaDoQuartoBimestre = 7;
 
-var somaDasNotas =
-  notaDoPrimeiroBimestre +
-  notaDoSegundoBimestre +
-  notaDoTerceiroBimestre +
-  notaDoQuartoBimestre;
+function enviar(){
+  var resposta = document.getElementById("nota_final"); 
+  var input1 = document.querySelector("#nota1");
+  var notaDoPrimeiroBimestre = Number(input1.value);
+  var input2 = document.querySelector("#nota2");
+  var notaDoSegundoBimestre = Number(input2.value);
+  var input3 = document.querySelector("#nota3");
+  var notaDoTerceiroBimestre = Number(input3.value);
+  var input4 = document.querySelector("#nota4");
+  var notaDoQuartoBimestre = Number(input4.value);
 
-var notaFinal = somaDasNotas / 4;
+  var somaDasNotas =
+    notaDoPrimeiroBimestre +
+    notaDoSegundoBimestre +
+    notaDoTerceiroBimestre +
+    notaDoQuartoBimestre;
 
-var notaFixada = notaFinal.toFixed(1);
+  var notaFinal = somaDasNotas / 4;
+  
+  var notaFixada = notaFinal.toFixed(1);
+  resposta.innerHTML = `Sua média final é : ${notaFixada}`;
+}
 
-console.log(`Bem vindo, ${nome}! Você teve uma nota final de: ${notaFixada}`);
+
+
 
 // Revisão
 // variáveis, strings, console.log, toFixed, oprerações, matemáticas, concatenação
